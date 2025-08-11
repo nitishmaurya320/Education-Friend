1795b26a3a4f320b458eed2b2c7d9234c819a4b8
Nitish Maurya <nitishmaurya2255@gmail.com>
2025-08-11 13:36:23 +0530

    ....

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -87,7 +87,7 @@ router.post("/login",async (req,res)=>{
             res.cookie("token", token, {
             httpOnly: true, 
             secure:true,
-            sameSite: "strict",
+            sameSite: "None",
             maxAge: 24 * 60 * 60 * 1000,
         });
         res.status(200).json({message:"Logged in Successfuly"})
